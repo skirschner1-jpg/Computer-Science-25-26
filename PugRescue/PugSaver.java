b0bd23bd51d1ec5015e5f3c4d231f8c6b709276c
GrasshoppersInc <grasshoppersbusiness@gmail.com>
2025-08-27 12:46:07 -0700

    Adding starter code

---
+++ PugRescue/PugSaver.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PugSaver {
+
+	//Moves every dog whose breed is "Pug" in the list to the back of the list
+	public static void rescuePugs(ArrayList<Dog> list) {
+
+	}
+}
