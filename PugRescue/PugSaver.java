28a85f6d4864344fc132909201e514cb2cb9079d
GrasshoppersInc <grasshoppersbusiness@gmail.com>
2025-08-27 13:27:44 -0700

    More Dog-Saving Progress

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -5,10 +5,18 @@ public class PugSaver {
 
 	//Moves every dog whose breed is "Golden Something" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
+		int end = list.size - 1;
 		for (int i = 0; i < list.size(); i++){
-			if (list.indexOf(i) == "Golden Retriever" || list.indexOf(i) == "Goldendoodle") {
-				list.add(list.get(i));
-				list.remove(i);
+			if (list.indexOf(i).contains("golden")) {
+				for(int j = list.size-1; j >0; j--){
+					if (!list(j).contains("golden")){
+						end = j;
+						j = 0;
+					}
+				}
+				Dog temp = list.get(end);
+				list.set(end, list.get(i));
+				list.set(i, temp);
 			}
 		}
 	}
