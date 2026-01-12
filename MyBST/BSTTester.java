7abf246e872ad95d6c5d800d342b9cb650a45718
GrasshoppersInc <grasshoppersbusiness@gmail.com>
2026-01-12 10:49:02 -0800

    Latest (01/12/26)

--- MyBST/BSTTester.java
+++ MyBST/BSTTester.java
@@ -1,15 +1,100 @@
 public class BSTTester {
     public static void main(String[] args) {
+        // MyBST test = new MyBST();
+        // test.add(20);
+        // test.add(15);
+        // test.add(25);
+        // test.add(10);
+        // test.add(18);
+        // test.add(8);
+        // test.add(12);
+        // test.add(11);
+        // test.add(30);
+        // // 9 things
+        // // test.remove(15);
+        // // test.add(2);
+        // // System.out.println(test.add(11));
+        // System.out.println("root (20): " + test.getRoot());
+        // System.out.println("root height (0): " + test.getRoot().getHeight());
+        // System.out.println("left of root(15): " + test.getRoot().getLeft());
+        // System.out.println("left of root height (1): " + test.getRoot().getLeft().getHeight());
+        // System.out.println("left of left (10): " + test.getRoot().getLeft().getLeft());
+        // System.out.println("left of left height (2): " + test.getRoot().getLeft().getLeft().getHeight());
+        // System.out.println("left of left of left (8): " + test.getRoot().getLeft().getLeft().getLeft());
+        // System.out.println(
+        //         "left of left of left height (3): " + test.getRoot().getLeft().getLeft().getLeft().getHeight());
+        // System.out.println("right of left of left (12): " + test.getRoot().getLeft().getLeft().getRight());
+        // System.out.println(
+        //         "right of left of left height (3): " + test.getRoot().getLeft().getLeft().getRight().getHeight());
+        // System.out.println(
+        //         "left of right of left of left (12): " + test.getRoot().getLeft().getLeft().getRight().getLeft());
+        // System.out.println(
+        //         "left of right of left of left height (4): "
+        //                 + test.getRoot().getLeft().getLeft().getRight().getLeft().getHeight());
+        // System.out.println("right of left (18): " + test.getRoot().getLeft().getRight());
+        // System.out.println("right of left height (2): " + test.getRoot().getLeft().getRight().getHeight());
+        // System.out.println("right of root (25): " + test.getRoot().getRight());
+        // System.out.println("right of root height (1): " + test.getRoot().getRight().getHeight());
+        // System.out.println("right of right (30): " + test.getRoot().getRight().getRight());
+        // System.out.println("right of right height (2): " + test.getRoot().getRight().getRight().getHeight());
+        // // System.out.println(test.toString());
+        
+        
+        // test.remove(15);
+        // System.out.println("\nAFTER REMOVE\n");
+        // System.out.println(test);
+        // System.out.println();
+        // // System.out.println(test.toString());
+        // System.out.println("root (20): " + test.getRoot());
+        // System.out.println("root height (0): " + test.getRoot().getHeight());
+        // System.out.println("left of root (12): " + test.getRoot().getLeft());
+        // System.out.println("left of root height (1): " + test.getRoot().getLeft().getHeight());
+        // System.out.println("left of left (10): " + test.getRoot().getLeft().getLeft());
+        // System.out.println("left of left height (2): " + test.getRoot().getLeft().getLeft().getHeight());
+        // System.out.println("left of left of left (8): " + test.getRoot().getLeft().getLeft().getLeft());
+        // System.out.println(
+        //         "left of left of left height (3): " + test.getRoot().getLeft().getLeft().getLeft().getHeight());
+        // System.out.println("right of left of left (11): " + test.getRoot().getLeft().getLeft().getRight());
+        // System.out.println(
+        //         "right of left of left height (3): " + test.getRoot().getLeft().getLeft().getRight().getHeight());
+        // System.out.println("right of left (18): " + test.getRoot().getLeft().getRight());
+        // System.out.println("right of left height (2): " + test.getRoot().getLeft().getRight().getHeight());
+        // System.out.println("right of root (25): " + test.getRoot().getRight());
+        // System.out.println("right of root height (1): " + test.getRoot().getRight().getHeight());
+        // System.out.println("right of right (30): " + test.getRoot().getRight().getRight());
+        // System.out.println("right of right height (2): " + test.getRoot().getRight().getRight().getHeight());
+
+        // System.out.println("\nORDERING\n");
+        // System.out.println(test);
+        // System.out.println("min: " + test.min());
+        // System.out.println("max: " + test.max());
+
+        testRemove();
+    }
+    
+    public static void testRemove() {
         MyBST test = new MyBST();
         test.add(20);
+        test.add(15);
+        test.add(25);
+        test.add(10); // remove doesn't work
+        test.add(18);
+        test.add(8);
         test.add(12);
-        test.add(30);
         test.add(11);
-        System.out.println(test.add(11));
-        System.out.println(test.getRoot());
-        System.out.println(test.getRoot().getLeft());
-        System.out.println(test.getRoot().getLeft().getLeft());
-        System.out.println(test.getRoot().getRight());
-        // System.out.println(test.toString());
+        test.add(30);
+        // System.out.println(test.remove(3));
+        // test.add(5);
+        // System.out.println(test.remove(3));
+        // test.add(3);
+        // test.add(2);
+        // System.out.println(test.remove(5));
+        System.out.println(test);
+        test.remove(10);
+        System.out.println(test);
+    }
+
+    public static void testRebalance(){
+        
     }
 }
--- MyBST/MyBST.java
+++ MyBST/MyBST.java
@@ -25,38 +25,43 @@ public class MyBST<E extends Comparable<E>> {
 	// Adds value to this BST, unless this tree already holds value.
 	// Returns true if value has been added; otherwise returns false.
 	public boolean add(E value) {
-		Comparable val = value;
 		BinaryNode toAdd = new BinaryNode(value);
 		if (root == null) {
 			root = toAdd;
+			toAdd.setHeight(0);
 			return true;
 		}
-		BinaryNode thisNode = root;
-		while (thisNode.hasLeft() && thisNode.hasRight()) {
-			if (val.compareTo(thisNode.getValue()) == 0) {
-				return false;
-			}
-			if (val.compareTo(thisNode.getValue()) < 0) {
-				thisNode = thisNode.getLeft();
-			} else if (val.compareTo(thisNode.getValue()) > 0) {
-				thisNode = thisNode.getRight();
-			} else {
-				// System.out.println("here");
-				// System.out.println();
-				return false;
-			}
-		}
+		return addHelper(root, toAdd, 1);
+		// PROBLEM: checking if it has left AND right so then once it doesn't, it moves on to adding the new node, but it might already have the place that the new node would take filled (like just left or just right)
+	}
+	
+	public boolean addHelper(BinaryNode thisNode, BinaryNode toAdd, int height) {
+		Comparable val = toAdd.getValue();
 		if (val.compareTo(thisNode.getValue()) < 0) {
-			thisNode.setLeft(toAdd);
-			return true;
+			if (thisNode.hasLeft()) { // skipping this but thisNode does have left??????? wait no it doesn't then why is it replacing :(
+				height++;
+				addHelper(thisNode.getLeft(), toAdd, height);
+			} else { //NEED ELSE, was doing both :(
+				thisNode.setLeft(toAdd);
+				toAdd.setParent(thisNode);
+				toAdd.setHeight(height); // HAVE TO ACTUALLY ADD TO HEIGHT! >:(, DONE i think
+				return true; 
+			}
 		}
-		if (val.compareTo(thisNode.getValue()) > 0) {
-			thisNode.setRight(toAdd);
-			return true;
+		else if (val.compareTo(thisNode.getValue()) > 0) {
+			if (thisNode.hasRight()) {
+				height++;
+				addHelper(thisNode.getRight(), toAdd, height);
+			} else {
+				thisNode.setRight(toAdd);
+				toAdd.setParent(thisNode);
+				toAdd.setHeight(height);
+				return true;
+			}
 		} else {
 			return false;
 		}
-		// PROBLEM: checking if it has left AND right so then once it doesn't, it moves on to adding the new node, but it might already have the place that the new node would take filled (like just left or just right)
+		return false;
 	}
 
 	// Removes value from this BST.  Returns true if value has been
@@ -64,34 +69,188 @@ public class MyBST<E extends Comparable<E>> {
 	// If removing a node with two children: replace it with the
 	//  largest node in the right subtree
 	public boolean remove(E value) {
-		return false;
+		if (root == null) {
+			return false;
+		}
+		BinaryNode toRemove = find(root, value);
+		if (toRemove == null) {
+			return false;
+		}
+		if (toRemove.getValue().equals(root.getValue()) && !toRemove.hasLeft() && !toRemove.hasRight()) {
+			root = null;
+			return true;
+		}
+
+		// removed node doesn't have children (easy, just cut it off)
+		if (!toRemove.hasLeft() && !toRemove.hasRight()) {
+			if (toRemove.getParent().getValue().compareTo(toRemove.getValue()) > 0) {
+				toRemove.getParent().setLeft(null);
+				return true;
+			} else {
+				toRemove.getParent().setRight(null);
+				return true;
+			}
+		}
+
+		// removed node has one child
+		// only left
+		if (toRemove.hasLeft() && !toRemove.hasRight()) {
+			if (toRemove.getValue().equals(root.getValue())) {
+				root = root.getLeft();
+				return true;
+			}
+			if (toRemove.getParent().getValue().compareTo(toRemove.getValue()) > 0) {
+				toRemove.getParent().setLeft(toRemove.getLeft());
+				return true;
+			} else {
+				toRemove.getParent().setRight(toRemove.getLeft());
+				return true;
+			}
+		}
+		// only right
+		if (toRemove.hasRight() && !toRemove.hasLeft()) {
+			if (toRemove.getParent().getValue().compareTo(toRemove.getValue()) > 0) {
+				toRemove.getParent().setLeft(toRemove.getRight());
+				return true;
+			} else {
+				toRemove.getParent().setRight(toRemove.getRight());
+				return true;
+			}
+		}
+
+		// finding replacement node
+		BinaryNode replacement = toRemove.getLeft();
+		while (replacement.hasRight()) {
+			replacement = replacement.getRight();
+		}
+
+		// 
+
+		// rehoming
+		BinaryNode replacementParent = replacement.getParent();
+		if (replacement.hasLeft()) {
+			decreaseHeight(replacement.getLeft());
+			replacementParent.setRight(replacement.getLeft());
+		} else {
+			replacementParent.setRight(null); // problematic if replacement is just one down from toRemove
+		}
+
+		// replacing val of remove
+		toRemove.setValue(replacement.getValue());
+
+		// cuts off toRemove
+		// if (toRemove.getParent().getValue().compareTo(toRemove.getValue()) > 0) {
+		// 	toRemove.getParent().setLeft(replacement);
+		// } else {
+		// 	toRemove.getParent().setRight(replacement);
+		// }
+		// if (toRemove.hasRight()) {
+		// 	replacement.setRight(toRemove.getRight());
+		// }
+
+		// if (!thisNode.hasLeft()) {
+		// 	if (thisNode.getParent().getValue().compareTo(thisNode.getValue()) > 0) {
+		// 		thisNode.getParent().setRight(null);
+		// 	} else {
+		// 		thisNode.getParent().setLeft(null);
+		// 	}
+		// }
+		// EDGE CASE: node getting promoted may have a node to the right!!! have to not just account here but also adjust heights // done:)
+		return true;
+	}
+	
+	public static void decreaseHeight(BinaryNode node) {
+		node.setHeight(node.getHeight() - 1);
+		if (node.hasLeft()) {
+			decreaseHeight(node.getLeft());
+		}
+		if (node.hasRight()) {
+			decreaseHeight(node.getRight());
+		}
+	}
+	
+	public BinaryNode find(BinaryNode thisNode, E value) {
+		if (thisNode.getValue().equals(value)) {
+			return thisNode;
+		}
+		if (thisNode.hasLeft()) {
+			if (find(thisNode.getLeft(), value) != null) {
+				return find(thisNode.getLeft(), value);
+			}
+		}
+		if (thisNode.hasRight()) {
+			if (find(thisNode.getRight(), value) != null) {
+				return find(thisNode.getRight(), value);
+			}
+		}
+		return null;
 	}
 	
 	// Returns the minimum in the tree
 	public E min() {
-		return null;
+		BinaryNode thisNode = root;
+		while (thisNode.hasLeft()) {
+			thisNode = thisNode.getLeft();
+		}
+		return (E) thisNode.getValue();
 	}
 	
 	// Returns the maximum in the tree.
 	public E max() {
-		return null;
+		BinaryNode thisNode = root;
+		while (thisNode.hasRight()) {
+			thisNode = thisNode.getRight();
+		}
+		return (E) thisNode.getValue();
 	}
+
+	public static boolean balance(BinaryNode node) {
+		// if left of node is heavier by more than 1 do rebalanceLeft & return true
+		// if right of node is heavier by more than 1 do rebalanceRight & return true
+		// else return false
+		return false;
+	}
+
+	public static void rebalanceLeft(BinaryNode node) {
+		BinaryNode curr = node;
+		BinaryNode leftOfNode = node.getLeft();
+		BinaryNode leftOfLeft = node.getLeft().getLeft();
+		node.setValue(leftOfNode.getValue());
+		node.setLeft(leftOfLeft);
+		node.setRight(curr);
+		// is assumption that since this is constantly being done, it will never be heavier by more than 2???
+	}
+	
+	public static void rebalanceRight(BinaryNode node) {
+		BinaryNode curr = node;
+		BinaryNode rightOfNode = node.getLeft();
+		BinaryNode rightOfRight = node.getLeft().getLeft();
+		node.setValue(rightOfNode.getValue());
+		node.setRight(rightOfRight);
+		node.setLeft(curr);
+	}
+
 
 	// Returns a bracket-surrounded, comma separated list of the contents of the nodes, in order
 	// e.g. [Apple, Cranberry, Durian, Mango]
 	public String toString() {
-		return "[" + toStringHelper(root) + "]";
+		if (root == null) {
+			return "[]";
+		}
+		String toReturn = toStringHelper(root, "");
+		toReturn = toReturn.substring(0, toReturn.length() - 2);
+		return "[" + toReturn + "]";
 	}
 
-	public String toStringHelper(BinaryNode thisNode){
+	public String toStringHelper(BinaryNode thisNode, String curr){
 		if (thisNode.hasLeft()) {
-			thisNode.getLeft().toString();
+			curr = toStringHelper(thisNode.getLeft(), curr);
 		}
-		// System.out.println(thisNode);
+		curr += thisNode + ", ";
 		if (thisNode.hasRight()) {
-			System.out.println(thisNode.getRight());
+			curr = toStringHelper(thisNode.getRight(), curr);
 		}
-		return "";
+		return curr;
 	}
 
 
